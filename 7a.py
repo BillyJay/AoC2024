b263881ca35d9eff7222f7ac1ab707e98ff1715e
BillyJay <billa.j@gmail.com>
2024-12-07 08:37:56 +0100

    Day 7

---
+++ 7a.py
@@ -0,0 +1,30 @@
+import collections
+from functools import cmp_to_key
+import sys
+from itertools import permutations, combinations_with_replacement, product
+from collections import deque 
+from copy import deepcopy
+
+file = open("input7.txt").read().strip()
+lines = file.split('\n')
+
+
+ans = 0
+for idx, l in enumerate(lines):
+	l = l.strip()
+	lhs = l.split(':')[0]
+	rhs = l.split(': ')[1]
+	rhs = list(map(int,rhs.split()))
+	a = ['+','*']
+	x = product(a,repeat = len(rhs)-1)
+
+	for y in x:
+		val = rhs[0]
+		for i in range(len(y)):
+			if (y[i]=='+'): val += rhs[i+1]
+			if (y[i]=='*'): val *= rhs[i+1]
+		if (val == int(lhs)):
+			got.append(idx)
+			ans += int(val)
+			break
+print(ans)
