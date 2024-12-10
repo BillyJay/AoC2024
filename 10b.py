874ec36cc52da0b66aaa60c6cc201739aaf7ce83
BillyJay <billa.j@gmail.com>
2024-12-10 07:39:59 +0100

    Day 10 improved

--- 10b.py
+++ 10b.py
@@ -6,23 +6,17 @@ C = len(lines[0])
 
 all_paths = set()
 
-def dfs(vis, field, i, j):
-	if (i,j) in vis: return 0
+def dfs(field, i, j):
 	if(field[i][j]==9 ): ans = 1
 	else: ans = 0
-	if(field[i][j]!=9): vis.add((i,j))
 	if(i+1<R and field[i][j]+1==field[i+1][j]):
-		ans += dfs(vis,field,i+1,j)
-		if (i+1,j) in vis: vis.remove((i+1,j))
+		ans += dfs(field,i+1,j)
 	if(j+1<C and field[i][j]+1==field[i][j+1]):
-		ans += dfs(vis,field,i,j+1)
-		if (i,j+1) in vis: vis.remove((i,j+1))
+		ans += dfs(field,i,j+1)
 	if(i-1>=0 and field[i][j]+1==field[i-1][j]):
-		ans += dfs(vis,field,i-1,j)
-		if (i-1,j) in vis: vis.remove((i-1,j))
+		ans += dfs(field,i-1,j)
 	if(j>=0 and field[i][j]+1==field[i][j-1]):
-		ans += dfs(vis,field,i,j-1)
-		if (i,j-1) in vis: vis.remove((i,j-1))
+		ans += dfs(field,i,j-1)
 	return ans
 
 
@@ -31,6 +25,5 @@ field = [[int(el) for el in l.strip()] for l in lines]
 for i in range(R):
 	for j in range(C):
 		if field[i][j] ==0:
-			vis = set()
-			ans += dfs(vis,field, i,j)
+			ans += dfs(field, i,j)
 print(ans)
